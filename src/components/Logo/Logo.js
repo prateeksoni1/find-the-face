a0323710b9679402f78c2ac3d72bac94b0bda924
proforty7 <proforty7@gmail.com>
2018-07-10 01:47:42 +0530

    Initial

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Tilt from 'react-tilt'
+import './Logo.css'
+import logo from './logo.png'
+
+const Logo = () => {
+	return (
+		<div className = 'ma4 mt0 ml6'>
+			<Tilt className="Tilt br-2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+		 	<div className="Tilt-inner pa4"> 
+		 		<img alt = 'logo' src = {logo} />
+		 	</div>
+			</Tilt>
+		</div>
+
+	);
+}
+
+export default Logo;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navigation = () => {
+	return (
+		<div>
+			<nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+				<p className = 'f3 link dim black underline pa3 pointer'>Sign out</p>
+			</nav>
+		</div>
+	);
+}
+
+export default Navigation;
